cec2922b26b27a7130e847a4ed279e36f2e278f7
hongyh <hongyh@hongyh_t219.conac.cn>
2016-03-17 14:10:22 +0800

    Thymeleaf研究

--- SB-Test/src/main/java/com/sb/hyh/web/HelloController.java
+++ SB-Test/src/main/java/com/sb/hyh/web/HelloController.java
@@ -1,20 +1,14 @@
 package com.sb.hyh.web;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @Value("${application.hello}")
-    private String hello;
-
     @RequestMapping("/hello")
     public String hello() {
         return "Hello Spring-Boot";
@@ -22,21 +16,9 @@ public class HelloController {
 
     @RequestMapping("/info")
     public Map<String, String> getInfo(@RequestParam String name) {
-        System.out.println(hello);
+        System.out.println(name);
         Map<String, String> map = new HashMap<String, String>();
         map.put("name", name);
         return map;
-    }
-
-    @RequestMapping("/list")
-    public List<Map<String, String>> getList() {
-        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-        Map<String, String> map = null;
-        for (int i = 1; i <= 5; i++) {
-            map = new HashMap<String, String>();
-            map.put("name", "Shanhy-" + i);
-            list.add(map);
-        }
-        return list;
     }
 }
