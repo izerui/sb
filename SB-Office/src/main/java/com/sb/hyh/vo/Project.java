83d67718d91d1a0d1483905370bb7b3a8c0af3ed
hong0220 <1023746826@qq.com>
2016-03-19 10:25:00 +0800

    增加office功能：到处到excel文档中

---
+++ SB-Office/src/main/java/com/sb/hyh/vo/Project.java
@@ -0,0 +1,40 @@
+package com.sb.hyh.vo;
+
+public class Project {
+	private Integer id;
+	private String name;
+	private String technology;
+	private String remarks;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTechnology() {
+		return technology;
+	}
+
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+}
