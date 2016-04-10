e41ea35645a7b0d950fffd33e15f2b34eb55b472
hong0220 <1023746826@qq.com>
2016-04-10 13:35:27 +0800

    修改ug

--- SB-Email/src/main/java/com/sb/hyh/Application.java
+++ SB-Email/src/main/java/com/sb/hyh/Application.java
@@ -2,10 +2,6 @@ package com.sb.hyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.sb.hyh.entities.h2.Post;
-import com.sb.hyh.repository.h2.PostRepository;
 
 @SpringBootApplication
 public class Application {
