4029b44dabc3558ff42d9d2da79ec616b701b8fa
hong0220 <1023746826@qq.com>
2016-04-10 15:51:04 +0800

    增加邮件操作

--- SB-Email/src/main/java/com/sb/hyh/Application.java
+++ SB-Email/src/main/java/com/sb/hyh/Application.java
@@ -2,11 +2,13 @@ package com.sb.hyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({ "classpath:application.properties", "classpath:mail.properties" })
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- SB-Email/src/main/java/com/sb/hyh/web/MailController.java
+++ SB-Email/src/main/java/com/sb/hyh/web/MailController.java
@@ -9,22 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sb.hyh.entity.EmailContent;
 import com.sb.hyh.service.mail.MailSenderService;
 
 @RestController
 public class MailController {
-	private Logger logger = Logger.getLogger(MailController.class.getName());
-	@Autowired
-	private MailSenderService mailSenderService;
+    private Logger logger = Logger.getLogger(MailController.class.getName());
+    @Autowired
+    private MailSenderService mailSenderService;
 
-	@RequestMapping(value = "/sendMail", method = RequestMethod.GET)
-	public String sendMail() {
-		try {
-			logger.info("sendMail");
-			mailSenderService.sendMail();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-		return "success";
-	}
+    @RequestMapping(value = "/sendMail", method = RequestMethod.GET)
+    public String sendMail() {
+        try {
+            logger.info("sendMail");
+            EmailContent emailContent = new EmailContent();
+            emailContent.setContent("测试中文");
+            emailContent.setTo("");
+            mailSenderService.sendMail(emailContent);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return "success";
+    }
 }
