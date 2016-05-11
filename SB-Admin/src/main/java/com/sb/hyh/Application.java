9032d6ec36f6c66b6323795389e0901ac3abbf1a
hong0220 <1023746826@qq.com>
2016-05-11 09:43:30 +0800

    提交代码

---
+++ SB-Admin/src/main/java/com/sb/hyh/Application.java
@@ -0,0 +1,15 @@
+package com.sb.hyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
