3c4f04e32cf1d28f487cf66c6fe9ebed27bc2e9e
hongyh <hongyh@hongyh_t219.conac.cn>
2016-05-20 16:07:16 +0800

    修改eruka

--- SB-Cloud-Eureka-Client/src/main/java/com/sb/hyh/EurekaServer.java
+++ SB-Cloud-Eureka-Client/src/main/java/com/sb/hyh/EurekaServer.java
@@ -1,11 +1,13 @@
 package com.sb.hyh;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 @EnableEurekaServer
+// @EnableEurekaClient
 public class EurekaServer {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServer.class, args);
---
+++ SB-Cloud-Eureka-Server/src/main/java/com/sb/hyh/web/ServiceController.java
@@ -0,0 +1,20 @@
+package com.sb.hyh.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceController {
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping("/")
+	public String hello() {
+		ServiceInstance localInstance = client.getLocalServiceInstance();
+		return "Hello World: " + localInstance.getServiceId() + ":" + localInstance.getHost() + ":"
+				+ localInstance.getPort();
+	}
+}
