36b5081b8d49f7ffb4946f6c93bbf7e11aac7187
hong0220 <1023746826@qq.com>
2016-06-11 12:16:55 +0800

    提交一波修改代码

--- SB-WechatAPI/src/main/java/com/wechat/WeChatApplication.java
+++ SB-WechatAPI/src/main/java/com/wechat/WeChatApplication.java
@@ -3,14 +3,12 @@ package com.wechat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "com.wechat")
+@SpringBootApplication
 public class WeChatApplication {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(WeChatApplication.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WeChatApplication.class, args);
+	}
 }
