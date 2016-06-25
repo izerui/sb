a9dcc09d2905c5ddd5ec7707b217e9cc944cbb39
hongyh <1023746826@qq.com>
2016-06-25 15:27:45 +0800

    整理一下代码

--- SB-H2/src/main/java/com/sb/hyh/entities/h2/Post.java
+++ SB-H2/src/main/java/com/sb/hyh/entities/h2/Post.java
@@ -8,61 +8,67 @@ import javax.persistence.Id;
 
 @Entity
 public class Post {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String title;
-    private String content;
-    private Date created;
-    private Date updated;
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String title;
+	private String content;
+	private Date created;
+	private Date updated;
 
-    public Post() {
+	public Post() {
 
-    }
+	}
 
-    public Post(String title, String content) {
-        this.title = title;
-        this.content = content;
-        this.created = this.updated = new Date();
-    }
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+		this.created = this.updated = new Date();
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public Date getCreated() {
-        return created;
-    }
+	public Date getCreated() {
+		return created;
+	}
 
-    public void setCreated(Date created) {
-        this.created = created;
-    }
+	public void setCreated(Date created) {
+		this.created = created;
+	}
 
-    public Date getUpdated() {
-        return updated;
-    }
+	public Date getUpdated() {
+		return updated;
+	}
 
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", title=" + title + ", content=" + content + ", created=" + created + ", updated="
+				+ updated + "]";
+	}
 }
