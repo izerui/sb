8bb77bcff54d254f1bd2a8d8f0784869d2dac3c7
hongyh <1023746826@qq.com>
2016-06-25 14:45:47 +0800

    修改下注释

--- SB-H2/src/main/java/com/sb/hyh/Application.java
+++ SB-H2/src/main/java/com/sb/hyh/Application.java
@@ -10,15 +10,15 @@ import com.sb.hyh.repository.h2.PostRepository;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        // 内存数据库操作
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        PostRepository postRepository = context.getBean(PostRepository.class);
-        postRepository.save(new Post("title1", "content1"));
-        postRepository.save(new Post("title2", "content2"));
-        postRepository.save(new Post("title3", "content3"));
-        postRepository.save(new Post("title4", "content4"));
-        postRepository.save(new Post("title5", "content5"));
-        System.out.println(postRepository.count());
-    }
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		PostRepository postRepository = context.getBean(PostRepository.class);
+		// 内存数据库操作
+		postRepository.save(new Post("title1", "content1"));
+		postRepository.save(new Post("title2", "content2"));
+		postRepository.save(new Post("title3", "content3"));
+		postRepository.save(new Post("title4", "content4"));
+		postRepository.save(new Post("title5", "content5"));
+		System.out.println(postRepository.count());
+	}
 }
